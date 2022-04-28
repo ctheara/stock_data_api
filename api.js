f2ddccce1a7c590b9b0db48415b2d962c65aaf42
ctheara <59755034+ctheara@users.noreply.github.com>
2022-04-28 19:43:02 -0400

    connect to API
    made connect to API

--- api.js
+++ api.js
@@ -1,32 +1,79 @@
+
+
+
 //connect to API and display data on page
 function getInfo() {
 
-    var data = null;
 
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
+   
+    const data = null;
+
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            recordStr = this.responseText; // JSON format
-            record = JSON.parse(recordStr); // Create JS object
+            recordStr = this.responseText;
+                    record = JSON.parse(recordStr); // Create JS object
 
-            //test to get first timestamp
-            msg = "<b><u>Result:</b></u> <br>Timestamp: " + record.chart.result[0].timestamp[0] +"<br>";
 
-            document.getElementById("info").innerHTML = msg;
+                    //test to get first timestamp
+                        msg = "<b><u>Result:</b></u> <br>Timestamp: " + record.chart.result[0].timestamp[0] +"<br>";
+
+                    //first timestamp
+                    let timeTest = record.chart.result[0].timestamp[9];
+                    var myDate = new Date(timeTest*1000);
+                    let dateArray = myDate.toLocaleString().split("/") +"<br>";
+
+                    let year = myDate.getFullYear();
+                    let month = myDate.getMonth();
+                    let day = myDate.getDay();
+
+                    msg += "Date: " +myDate +"<br>";
+                    msg += year +"<br>";
+                    msg += month +"<br>";
+                    msg += day +"<br>";
+
+                    //test to get one timestamp
+                    msg += "<b><u>Result:</b></u> <br>Price: " + record.chart.result[0].indicators.quote[0].open[0] +"<br>";
+
+
+
+                    let dataPointList = [];
+
+
+                    //test to get all price
+                    msg += "Prices: ";
+                        for (var i = 0; i < record.chart.result[0].indicators.quote[0].open.length; i++) {
+                            //msg += record.chart.result[0].indicators.quote[0].open[i];
+                            let price = record.chart.result[0].indicators.quote[0].open[i].toString().slice(0,4);
+                            dataPointList.push({y:parseFloat(price)});
+                            //msg += price
+                            if (i + 1 != record.chart.result[0].indicators.quote[0].open.length) {
+                                msg += "";
+                            }
+                        }
+
+                    msg += dataPointList[8].y;
+
+
+                    document.getElementById("info").innerHTML = msg;
 
         }
         else{
-            alert('error');
-            document.getElementById("info").innerHTML = "ERROR"
+            
         }
     }
 
+    var api_key = "913846a913msh9f1d8abf8e98d47p1e0c1djsn54327b4fa572";
+
+    
     xhr.open("GET", "https://yh-finance.p.rapidapi.com/stock/v2/get-chart?interval=1d&symbol=AMRN&range=5y&region=US");
     xhr.setRequestHeader("X-RapidAPI-Host", "yh-finance.p.rapidapi.com");
-    xhr.setRequestHeader("X-RapidAPI-Key", "913846a913msh9f1d8abf8e98d47p1e0c1djsn54327b4fa572");
+    xhr.setRequestHeader("X-RapidAPI-Key", api_key);
 
     xhr.send(data);
 
 };
+
+
